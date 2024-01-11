c77c5aed458835279fd238f123dc18a95630e424
emmy <52672675+emmaccode@users.noreply.github.com>
2024-01-11 05:51:45 -0700

    base `assign!`

--- src/ParametricProcesses.jl
+++ src/ParametricProcesses.jl
@@ -6,6 +6,18 @@ abstract type AbstractWorker end
 abstract type Process end
 abstract type WorkerProcess <: Process end
 abstract type Threaded <: Process end
+abstract type AbstractJob end
+
+mutable struct ProcessJob <: AbstractJob
+    f::Function
+    args
+    kwargs
+    function ProcessJob(f::Function, args ...; keyargs ...)
+        new(f, args, keyargs)
+    end
+end
+
+const new_job = ProcessJob
 
 mutable struct Worker{T <: Process} <: AbstractWorker
     name::String
@@ -43,19 +55,19 @@ mutable struct ProcessManager
         workers::Vector{Worker} = [w for w in workers]
         new(workers, fs)
     end
-    function ProcessManager(workers::Vector{<:AbstractWorker}) where {T <: Process}
+    function ProcessManager(workers::Vector{<:AbstractWorker})
         fs = Vector{Pair{LoadDistributor, Function}}()
         new(workers, fs)
     end
 end
 
-function show(io::IO, pm::ProcessManager{<:Any})
+function show(io::IO, pm::ProcessManager)
     println("pid | process type | name | active")
     [show(io, worker) for worker in pm.workers]
 end
 
 
-function getindex(pm::ProcessManager{<:Any}, name::String)
+function getindex(pm::ProcessManager, name::String)
     pos = findfirst(worker::Worker{<:Any} -> worker.name == name, pm.workers)
     if isnothing(pos)
         # throw
@@ -63,7 +75,7 @@ function getindex(pm::ProcessManager{<:Any}, name::String)
     pm.workers[pos]
 end
 
-function getindex(pm::ProcessManager{<:Any}, pid::Int64)
+function getindex(pm::ProcessManager, pid::Int64)
     pos = findfirst(worker::Worker{<:Any} -> worker.pid == pid, pm.workers)
     if isnothing(pos)
         # throw
@@ -77,16 +89,41 @@ function create_workers(n::Int64, of::Type{Threaded},
     [Worker{of}(names[e], pid) for (e, pid) in enumerate(pids)]
 end
 
-function add_workers!(pm::ProcessManager, n::Int64, of::Type{<:Process} = Threaded)
-
+function add_workers!(pm::ProcessManager, n::Int64, of::Type{<:Process} = Threaded, names::String ...)
+    workers = Vector{Worker{<:Any}}()
+    if name_n == 0
+        workers = create_workers(n, of)
+    elseif name_n != n
+        # throw
+    else
+        workers = create_workers(n, of, [name for name in names])
+    end
+    pm.workers = vcat(pm.workers, workers)
 end
+
+function delete!(pm::ProcessManager, pid::Int64)
+    pos = findfirst(worker::Worker{<:Any} -> worker.pid == pid, pm.workers)
+    if isnothing(pos)
+        # throw
+    end
+    deleteat!(pm.workers, pos)
+end
+
+function delete!(pm::ProcessManager, name::String)
+    pos = findfirst(worker::Worker{<:Any} -> worker.pid == pid, pm.workers)
+    if isnothing(pos)
+        # throw
+    end
+    deleteat!(pm.workers, pos)
+end
+
 
 function processes(n::Int64, of::Type{<:Process} = Threaded, names::String ...)
     workers = Vector{Worker{<:Any}}()
     name_n = length(names)
     if name_n == 0
         workers = create_workers(n, of)
-    elseif name_n != N
+    elseif name_n != n
         # throw
     else
         workers = create_workers(n, of, [name for name in names])
@@ -94,24 +131,26 @@ function processes(n::Int64, of::Type{<:Process} = Threaded, names::String ...)
     ProcessManager(workers)
 end
 
-function assign!(f::Function, pm::ProcessManager, nsorpids::Tuple, args ...; keyargs ...)
+function assign!(f::Function, pm::ProcessManager, pid::Int64, job::AbstractJob)
+    assigned_task = remotecall(job.f, pid, job.args ...; job.kwargs ...)
+    assigned_worker = pm[pid]
+    assigned_worker.active = true
+    @async begin
+        wait(assigned_task)
+        f(fetch(assigned_task))
+        assigned_worker.active = false
+    end
+end
+
+function distribute!(pm::ProcessManager, job::AbstractJob ...)
 
 end
 
-function distribute!(pm::ProcessManager{}, fs::Pair{Function, Tuple} ...; keyargs ...)
+function distribute!(pm::ProcessManager, jobs::Pair{Float64, <:AbstractJob} ...)
 
 end
 
-function distribute!(f::Function, pm::ProcessManager{})
-
-end
-
-
-function kill!()
-
-end
-
-export processes, assign!, distribute!, Worker, ProcessManager
-export Async, Threads
+export processes, add_workers!, assign!, distribute!, Worker, ProcessManager
+export Threaded
 
 end # module BasicProcesses
